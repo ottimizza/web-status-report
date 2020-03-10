438347e7dd4d27ca314875c5ceb26fea0602b207
Rodrigo <rodrigo.h.m.112200@gmail.com>
2020-03-10 14:27:53 -0300

    breadcrumb

--- src/app/modules/integrateds/integrateds.module.ts
+++ src/app/modules/integrateds/integrateds.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IntegratedRouting } from './integrated.routing';
 import { IntegratedListComponent } from './page/integrateds-list/integrateds-list.component';
+import { BreadcrumbModule } from '@shared/components/breadcrumb/breadcrumb.module';
 
 @NgModule({
   declarations: [IntegratedListComponent],
-  imports: [IntegratedRouting, CommonModule]
+  imports: [IntegratedRouting, CommonModule, BreadcrumbModule]
 })
 export class IntegratedModule {}
--- src/app/modules/integrateds/page/integrateds-list/integrateds-list.component.ts
+++ src/app/modules/integrateds/page/integrateds-list/integrateds-list.component.ts
@@ -1,7 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FakeApiService } from '@app/http/fake-api.serive';
+import { PageInfo } from '@shared/models/GenericPageableResponse';
+import { BreadCrumb } from '@shared/components/breadcrumb/breadcrumb.component';
 
 @Component({
   templateUrl: './integrateds-list.component.html',
   styleUrls: ['./integrateds-list.component.scss']
 })
-export class IntegratedListComponent {}
+export class IntegratedListComponent implements OnInit {
+  dataSource: any[];
+  pageInfo: PageInfo;
+
+  breadcrumb: BreadCrumb = {
+    label: 'Voltar',
+    url: ''
+  };
+
+  constructor(private _service: FakeApiService) {}
+
+  ngOnInit(): void {
+    this._service.getIntegratedOrganizations().subscribe(int => {
+      this.pageInfo = int.pageInfo;
+      this.dataSource = int.records;
+    });
+  }
+}
