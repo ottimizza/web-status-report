3ec93f360580120d1af2abe574cc5733bff7a6dd
zuanz <ruanrgm@gmail.com>
2020-03-10 11:07:24 -0300

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+
+import { AuthGuard } from '@app/guard/auth.guard';
+import { NoAuthGuard } from '@app/guard/no-auth.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    data: {
+      breadcrumb: 'Dashboard'
+    },
+    component: ContentLayoutComponent,
+    canActivate: [NoAuthGuard], // Should be replaced with actual auth guard
+    children: []
+  },
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    loadChildren: () => import('@modules/auth/auth.module').then(m => m.AuthModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true,
+  oauthBaseUrl: 'https://development-oauth-server.herokuapp.com',
+  oauthClientId: '58a94562a7a7502d4c1b',
+  imageCompressionBaseUrl: 'https://development-image-compression.herokuapp.com',
+  storageBaseUrl: 'https://s4.ottimizzacontabil.com:55325',
+  storageApplicationId: 'account-avatar',
+  storageAccountingId: 'ottimizza',
+  applicationId: 'ottimizza',
+  firebase: {
+    apiKey: 'AIzaSyAPjrzkQyjYwq0eXRavaA9LS_rrt9p5XgE',
+    authDomain: 'portal-4c1f7.firebaseapp.com',
+    databaseURL: 'https://portal-4c1f7.firebaseio.com',
+    projectId: 'portal-4c1f7',
+    storageBucket: 'portal-4c1f7.appspot.com',
+    messagingSenderId: '92294481127',
+    appId: '1:92294481127:web:97256da8c44df6c0b48e42',
+    measurementId: 'G-ZLXKYRNY7Z'
+  }
+};
