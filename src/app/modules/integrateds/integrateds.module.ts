d14cbf08bdf2ca09ef5099714157e8ebba51165b
Rodrigo <rodrigo.h.m.112200@gmail.com>
2020-03-10 15:03:02 -0300

    Complex filter

--- src/app/modules/integrateds/integrateds.module.ts
+++ src/app/modules/integrateds/integrateds.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 import { IntegratedRouting } from './integrated.routing';
 import { IntegratedListComponent } from './page/integrateds-list/integrateds-list.component';
 import { BreadcrumbModule } from '@shared/components/breadcrumb/breadcrumb.module';
+import { ComplexSearchModule } from '@shared/components/search/complex-search.module';
+import { MatChipsModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [IntegratedListComponent],
-  imports: [IntegratedRouting, CommonModule, BreadcrumbModule]
+  imports: [
+    IntegratedRouting,
+    CommonModule,
+    BreadcrumbModule,
+    ComplexSearchModule,
+    MatChipsModule,
+    MatIconModule
+  ]
 })
 export class IntegratedModule {}
